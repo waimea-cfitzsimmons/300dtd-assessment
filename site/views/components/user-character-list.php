da54ba3c8b447241813a20901bac897c26d94d95
waimea-cfitzsimmons <cfitzsimmons@waimea.school.nz>
2024-07-02 12:17:05 +1200

    attempting things

---
+++ site/views/components/user-character-list.php
@@ -0,0 +1,36 @@
+<!-- Generate a list of things, e.g. from a database -->
+
+<ul>
+
+<?php
+require_once "lib/db.php";
+$db = connectToDB();
+
+echo '<h1>' . $users['id'] . '</h1>';
+
+// Get the picture associated info, but NOT the image data
+$query = 'SELECT id, name FROM characters WHERE creator = ?';
+
+try {
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $characters = $stmt->fetchAll();
+}
+catch (PDOException $e) {
+    consoleError($e->getMessage(), 'DB Fetch Things');
+    die('There was an error getting things from the database');
+}
+
+// Work through all pictures
+foreach ($characters as $character) {
+
+
+    echo '<li
+    hx-trigger="click"
+    hx-get="/character/' . $character['id'] . '"
+    hx-target="#character-info"
+>';
+echo $character['name'];
+echo '</li>';
+}
+?>
--- site/views/components/user-preview.php
+++ site/views/components/user-preview.php
@@ -1,21 +1,54 @@
 <?php
 require_once "lib/db.php";
-require_once 'lib/session.php';
+require_once "lib/session.php";
 $db = connectToDB();
-
 // Get the image type and binary data
-$query = 'SELECT id
-                 username,
+$query = 'SELECT id,
+                 username
 FROM users
 WHERE id=?';
 
 try {
     $stmt = $db->prepare($query);
     $stmt->execute([$id]);
-    $character = $stmt->fetch();
+    $user = $stmt->fetch();
 
 }
 catch (PDOException $e) {
     consoleError($e->getMessage(), 'DB Fetch things error');
     die('There was an error getting things from data base');
 }
+
+
+// $query = 'SELECT characters.id AS charID, 
+//                  characters.name,
+//                  characters.creator,
+//                  users.id AS userID
+// FROM characters 
+// JOIN users 
+// ON characters.creator = users.id 
+// WHERE characters.creator=?';
+
+// try {
+//     $stmt = $db->prepare($query);
+//     $stmt->execute([$id]);
+//     $characters = $stmt->fetch();
+// }
+
+// catch (PDOException $e) {
+//     consoleError($e->getMessage(), 'DB Fetch things error');
+//     die('There was an error getting things from data base');
+// }
+
+echo '<h1>' . $user['username'] . '</h1>';
+echo '<h2> List of Characters: </h2>';
+// foreach ($characters as $character) {
+//     echo'<h3>character</h3>';
+// }
+
+?>
+<div
+    id="character-list"
+    hx-get="/character-list/.$users['id']."
+    hx-trigger="load"
+></div>
